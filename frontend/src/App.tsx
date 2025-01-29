7549cdb934ad2dc86f05cd0785ae7c581fd7b563
Eduardo Gerhardt <comercial.eduardogerhardt@gmail.com>
2025-01-29 11:37:30 -0300

    feat: add shadcn/ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
     <>
       <div className="items-center justify-center flex mt-10">
-        <h1 className="font-black text-5xl">Vite + React + Typescript + Tailwind</h1>
+          <h1 className="font-black text-5xl">Vite + React + Typescript + Tailwind +</h1>
+          <Button variant="destructive" className="mx-2 font-black text-xl">Shadcn/ui</Button>
       </div>
     </>
   )
