6d72bc7318bf5920977df8d9f151cb0bb0047fe3
Eduardo Gerhardt <comercial.eduardogerhardt@gmail.com>
2025-01-29 11:11:56 -0300

    feat: add react frontend environment with ts and tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="items-center justify-center flex mt-10">
+        <h1 className="font-black text-5xl">Vite + React + Typescript + Tailwind</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
